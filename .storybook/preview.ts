4649fbc328bda3464b18d9a7dc57ed395757bd9d
Ryan DeBeasi <ryan.debeasi@libertymutual.com>
2025-09-30 11:52:07 -0400

    Remove unused code

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,12 +2,7 @@ import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
+    previewTabs: false
   },
 };
 
